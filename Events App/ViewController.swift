5d5a4237a9acb39f17c29a5726a588165603687a
Adetunji Dosunmu <dosunmu5@flg05-mac-10.coventry.ac.uk>
2017-12-06 16:31:07 +0000

    Events App 06.12.17

--- Events App/ViewController.swift
+++ Events App/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var dropDown: UIPickerView!
+    
+    //create list
+    var list = ["Wedding", "Birthday Party", "Graduation Celebration", "Baby Shower", "Anniversary", "Other"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +26,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+        
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleforRow row: Int, forComponent component: Int) -> String! {
+        self.view.endEditing(true)
+        return list[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row:Int, inComponent component: Int) {
+        self.textBox.text = self.list[row]
+        self.dropDown.isHidden = true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField)
+        {
+            if textField == self.textBox {
+                self.dropDown.isHidden = false
+            }
+    }
 }
 
